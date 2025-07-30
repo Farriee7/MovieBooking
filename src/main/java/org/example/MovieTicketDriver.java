ac57bee07539e52d3eaa67c8cba8b722e13b0778
Soumyadeep Chatterjee <91474923+Farriee7@users.noreply.github.com>
2025-07-31 04:04:31 +0530

    Update MovieTicketDriver.java

--- src/main/java/org/example/MovieTicketDriver.java
+++ src/main/java/org/example/MovieTicketDriver.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class MovieTicketDriver {
     public static void main(String[] args){
-        Movie m = new Movie("Dabangg","Action",4.7, 126,"Its a good movie about a Mumbai Cop (Salman Khan)");
-        m.insertMovie();
-        m.showMovies();
+       new LoginWindow();
     }
 }
